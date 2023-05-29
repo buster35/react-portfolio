8c3083acf4b9c03ca62458c216496102a31d53d0
Sleepy Cat <gilb2966@gmail.com>
2023-05-29 18:44:56 -0500

    debugged list key issue

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,26 +6,28 @@ import Row from "react-bootstrap/Row";
 
 function Project({ projects }) {
   return (
-    <Row className="g-4">
-      <h2 id="portfolio">Portfolio:</h2>
-      {projects.map((project) => (
-        <Col>
-          <Card style={{ width: "18rem" }} key={project.name}>
-            <Card.Img variant="top" src={project.cardBg} />
-            <Card.Body>
-              <Card.Title>{project.name}</Card.Title>
-              <Card.Text>{project.projectType}</Card.Text>
-              <Button variant="secondary" href={project.gitHub}>
-                GitHub Repo
-              </Button>
-              <Button variant="secondary" href={project.gitHub}>
-                Demo
-              </Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      ))}
-    </Row>
+    <section>
+      <Row className="g-4">
+        <h2 id="portfolio">Portfolio:</h2>
+        {projects.map((project) => (
+          <Col key={project.id}>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={project.cardBg} />
+              <Card.Body>
+                <Card.Title>{project.name}</Card.Title>
+                <Card.Text>{project.projectType}</Card.Text>
+                <Button variant="secondary" href={project.gitHub}>
+                  GitHub Repo
+                </Button>
+                <Button variant="secondary" href={project.gitHub}>
+                  Demo
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </section>
   );
 }
 
--- src/projects.js
+++ src/projects.js
@@ -1,11 +1,13 @@
 const projects = [
   {
+    id: 0,
     name: "Ecommerce Backend",
     projectType: "MySQL Restful Demo App",
     gitHub: "https://github.com/buster35/ecommerce-backend",
     cardBg: "insomnia-screengrab.png",
   },
   {
+    id: 1,
     name: "Note-Taker App",
     projectType: "Front-End Local Storage Demo",
     gitHub: "https://github.com/buster35/note-taker-app",
