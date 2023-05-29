a4d1397e9dfb8bd51ea9d8d0480c471af95305e4
Sleepy Cat <gilb2966@gmail.com>
2023-05-28 20:51:46 -0500

    continued building src and assets directories

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,6 @@
 import React from "react";
 import Navigation from "./Navigation";
 
-//TODO: WHEN I view the header
-// THEN I am presented with the developer's name and navigation with titles corresponding to different sections of the
-//TODO: WHEN I view the navigation titles
-// THEN I am presented with the titles About Me, Portfolio, Contact, and Resume, and the title corresponding to the current section is highlighted
-
 // The `Header` component is a child of our `App` component and is responsible for rendering the header across different pages//
 function Header(props) {
   const name = "Zach Gilbert";
--- src/projects.js
+++ src/projects.js
@@ -3,6 +3,12 @@ const projects = [
     name: "Ecommerce Backend",
     projectType: "MySQL Restful Demo App",
     gitHub: "https://github.com/buster35/ecommerce-backend",
-    cardBg: "./assets/insomnia-screengrab.png",
+    cardBg: "../assets/insomnia-screengrab.png",
+  },
+  {
+    name: "Note-Taker App",
+    projectType: "Front-End Local Storage Demo",
+    gitHub: "https://github.com/buster35/note-taker-app",
+    cardBg: "../assets/Note-persistance.png",
   },
 ];
