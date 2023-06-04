599a64a1c0cd0e8373165e9d7a51ced6969f79c7
Sleepy Cat <gilb2966@gmail.com>
2023-06-03 19:51:57 -0500

    added modal functionality

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,26 +1,49 @@
 import React from "react";
-import { Button, Card, Col, Row, Container } from "react-bootstrap";
+import { useState } from "react";
+import {
+  Button,
+  Card,
+  Col,
+  Row,
+  Container,
+  Modal,
+  Image,
+} from "react-bootstrap";
 
 function Project({ projects }) {
-  //not logging bc we aren't rendering
-  console.log({ projects });
-  console.log("test");
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => {
+    // console.log(e.target);
+    // projects.cardBg = getAttribute("src");
+    setShowModal(true);
+  };
+
   return (
     <Container fluid>
-      <Row className="g-4 pages">
+      <Row className="g-4 mt-0 pages">
         <h2 id="portfolio">Portfolio:</h2>
         {projects.map((project) => (
           <Col key={project.id} style={{ padding: "10px", margin: "20px" }}>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img
-                className="cardImg"
-                variant="top"
-                src={project.cardBg}
-                alt="project screenshots"
-              />
-              <Card.Body style={{ backgroundColor: "#778DA9" }}>
+            <Card style={{ width: "auto" }}>
+              <a onClick={handleShow}>
+                <Card.Img
+                  className="cardImg"
+                  variant="top"
+                  src={project.cardBg}
+                  alt="project screenshots"
+                />
+              </a>
+              <Card.Body
+                style={{
+                  backgroundColor: "#778DA9",
+                }}
+              >
                 <Card.Title>{project.name}</Card.Title>
+
                 <Card.Text>{project.projectType}</Card.Text>
+
                 <Button variant="secondary" href={project.gitHub}>
                   GitHub Repo
                 </Button>
@@ -29,6 +52,28 @@ function Project({ projects }) {
                 </Button>
               </Card.Body>
             </Card>
+            <Modal
+              show={showModal}
+              onHide={handleClose}
+              backdrop="static"
+              keyboard={false}
+              style={{ backgroundColor: "#0D1B2A" }}
+            >
+              <Modal.Header closeButton>
+                <Modal.Title>{project.name}</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <Image
+                  src={project.cardBg}
+                  style={{ maxWidth: "450px", objectFit: "contain" }}
+                ></Image>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+              </Modal.Footer>
+            </Modal>
           </Col>
         ))}
       </Row>
