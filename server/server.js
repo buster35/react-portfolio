b3454283b8d8093db55b0ca94dd431e686649f4c
Sleepy Cat <gilb2966@gmail.com>
2023-06-21 17:47:22 -0500

    hi

--- server/server.js
+++ server/server.js
@@ -8,13 +8,12 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(routes);
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
-
-app.use(routes);
 
 db.once("open", () => {
   app.listen(PORT, () => console.log(`Now listening on localhost: ${PORT}`));
