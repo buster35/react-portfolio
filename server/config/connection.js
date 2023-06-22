9fc74c56cdb9f36b55f6828013330b8d1f6a55b2
Sleepy Cat <gilb2966@gmail.com>
2023-06-21 20:43:42 -0500

    debugging heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,14 +1,10 @@
 const mongoose = require("mongoose");
 
 //Wrap mongoose around local connection to MongoDB
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/react-portfolio",
-  {
-    dbName: "react-portfolio",
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //Export our db connection
 module.exports = mongoose.connection;
