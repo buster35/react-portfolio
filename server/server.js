7213346d660c1e95e30f017c24015f079b0dfd09
Sleepy Cat <gilb2966@gmail.com>
2023-06-21 20:29:36 -0500

    required in path for routing

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const path = require("path");
+const path = require("path");
 const db = require("./config/connection");
 const routes = require("./routes");
 
