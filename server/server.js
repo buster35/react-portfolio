cea286ce465c073f39571bff63446beb71ef54af
Sleepy Cat <gilb2966@gmail.com>
2023-06-22 11:11:54 -0500

    updating server.js

--- server/server.js
+++ server/server.js
@@ -16,6 +16,11 @@ if (process.env.NODE_ENV === "production") {
   app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "..", "client/build/index.html"));
   });
+
+  app.get("*", function (req, res) {
+    const index = path.join(__dirname, "build", "index.html");
+    res.sendFile(index);
+  });
 }
 
 app.use(routes);
