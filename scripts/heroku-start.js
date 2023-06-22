a63aaa876351e7b4ef0d44b3a42ebe469c75a221
Sleepy Cat <gilb2966@gmail.com>
2023-06-22 11:53:11 -0500

    debugging

--- scripts/heroku-start.js
+++ scripts/heroku-start.js
@@ -13,10 +13,9 @@ app.get("/", function (req, res) {
 // app.get("*", function (req, res) {
 //   res.sendFile(path.join(__dirname, "..", "build/index.html"));
 // });
-
-app.get("/*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "..", "client/build/index.html"));
-});
+// app.get("/*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "..", "client/build/index.html"));
+// });
 
 // app.get("*", function (req, res) {
 //   const index = path.join(__dirname, "build", "index.html");
