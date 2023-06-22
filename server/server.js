35e23f703651686569cea703c294d3db7215d4b3
Sleepy Cat <gilb2966@gmail.com>
2023-06-22 10:32:33 -0500

    debugging heroku

--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
@@ -18,6 +17,8 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "..", "client/build/index.html"));
   });
 }
+
+app.use(routes);
 
 //This sets our express "init" for localhost (or other via process.env.PORT) inside a method that first initiates our connection to the mongodb database//
 db.once("open", () => {
