0b5ca4d482d14466232004a131c009e61b2920a0
Sleepy Cat <gilb2966@gmail.com>
2023-06-22 11:21:14 -0500

    routing

--- scripts/heroku-start.js
+++ scripts/heroku-start.js
@@ -4,24 +4,24 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 // Your static pre-build assets folder
-app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static(path.join(__dirname, "..", "client/build/index.html")));
 // Root Redirects to the pre-build assets
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "..", "build"));
+  res.sendFile(path.join(__dirname, "..", "client/build/index.html"));
 });
 // Any Page Redirects to the pre-build assets folder index.html that // will load the react app
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "..", "build/index.html"));
-});
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "..", "build/index.html"));
+// });
 
 app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "..", "client/build/index.html"));
 });
 
-app.get("*", function (req, res) {
-  const index = path.join(__dirname, "build", "index.html");
-  res.sendFile(index);
-});
+// app.get("*", function (req, res) {
+//   const index = path.join(__dirname, "build", "index.html");
+//   res.sendFile(index);
+// });
 
 app.listen(port, () => {
   console.log("Server is running on port: ", port);
