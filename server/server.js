9973f9ab2b8f8d224bb40a041fa226a1819a04e2
Sleepy Cat <gilb2966@gmail.com>
2023-06-22 11:40:48 -0500

    updating server.js

--- server/server.js
+++ server/server.js
@@ -10,18 +10,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../client/build")));
 
-  app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "..", "../client/build/index.html"));
-  });
+//   app.get("/*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "..", "../client/build/index.html"));
+//   });
 
-  app.get("*", function (req, res) {
-    const index = path.join(__dirname, "build", "../client/build/index.html");
-    res.sendFile(index);
-  });
-}
+//   app.get("*", function (req, res) {
+//     const index = path.join(__dirname, "build", "../client/build/index.html");
+//     res.sendFile(index);
+//   });
+// }
 
 app.use(routes);
 
